f6d2a9515b22bcfa954c9b449bd93a15147e73a9
Pedra96 <darkraidialga@yahoo.it>
2022-11-29 14:41:44 +0100

    Add project files.

---
+++ CSharpShop/Prodotto_Classe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop {
+    public class Prodotto_Classe {
+        private string Nome;
+        private string descrizione;
+        private double prezzo;
+        private float iva;
+
+
+
+        public void SetNome(string nome) {
+            this.Nome = nome;
+        }
+    }
+
+ 
+}
