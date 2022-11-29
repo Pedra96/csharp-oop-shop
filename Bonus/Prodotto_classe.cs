07b9f5ac3d78ff50e2a5525eff2d5179d23f28bd
Pedra96 <darkraidialga@yahoo.it>
2022-11-29 17:16:42 +0100

    bonus completato

---
+++ Bonus/Prodotto_classe.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus {
+    internal class Prodotto_classe {
+
+        private string codice;
+        private string Nome;
+        private string descrizione;
+        private double prezzo;
+        private float iva;
+        public Prodotto_classe(string nome, double prezzo, float iva, string descrizione = "") {
+            this.codice = this.SetCodice();
+            this.Nome = nome;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+        public void SetNome(string nome) {
+            this.Nome = nome;
+        }
+        public string GetNome() {
+            return this.Nome;
+        }
+        public void SetDescrizione(string descrizione) {
+            this.descrizione = descrizione;
+        }
+        public string GetDescrizione() {
+            return this.descrizione;
+        }
+        public double CalcoloPrezzoConIva() {
+            float iva = this.GetIva();
+            double prezzo = this.GetPrezzo();
+            return Math.Round(prezzo + ((prezzo * iva) / 100),2);
+        }
+        public void SetPrezzo(double prezzo) {
+            Math.Round(prezzo, 2);
+            this.prezzo = prezzo;
+        }
+        public double GetPrezzo() {
+            return this.prezzo;
+        }
+        public void SetIva(float iva) {
+            Math.Round(iva, 2);
+            this.iva = iva;
+        }
+        public float GetIva() {
+            return this.iva;
+        }
+        private string SetCodice() {
+            Random number = new Random();
+            string numero = Convert.ToString(number.Next(100000000));
+            if (numero.Length < 8) {
+                string pad ="";
+                for (int i = 0; i < (8 - numero.Length) ; i++) {
+                    pad += '0';
+                }
+            string numeroCompleto = pad + numero;
+            return numeroCompleto;
+            } else {
+                return numero;
+            }
+            
+        }
+        public string GetCodice() {
+            return this.codice;
+        }
+        public void StampaProdottoASchermo() {
+            Console.WriteLine("------------------");
+            Console.WriteLine($"Nome del prodotto: {this.Nome}");
+            Console.WriteLine($"Descrizione: {this.descrizione}");
+            Console.WriteLine($"prezzo senza iva: {this.prezzo} euro");
+            Console.WriteLine($"iva prodotto: {this.iva} %");
+            Console.WriteLine($"prezzo con iva: {this.CalcoloPrezzoConIva()} euro");
+            Console.WriteLine($"codice prodotto: {this.codice}{this.Nome}");
+            Console.WriteLine("------------------");
+        }
+    }
+
+}
---
+++ Bonus/Program.cs
@@ -0,0 +1,17 @@
+﻿/*BONUS:
+create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+Usando una List, dichiarate una lista dei prodotti di un negozio e inseriteci al suo interno qualche prodotto che vi aspettate di trovare nel negozio.
+Stampate poi l’elenco dei vostri prodotti che avete previsto nel negozio.*/
+
+
+using Bonus;
+
+var prodotto1 = new Prodotto_classe("Banana",5,4,"bananane molto buone");
+var prodotto2 = new Prodotto_classe("Pomodori", 4.3, 4, "pomodori belli rossi");
+var prodotto3 = new Prodotto_classe("Gelato", 10.40, 10,"Gelato al fior di latte");
+
+var Wallmart=new List<Prodotto_classe>() { prodotto1,prodotto2,prodotto3};
+
+foreach(var articolo in Wallmart) {
+    articolo.StampaProdottoASchermo();
+}
